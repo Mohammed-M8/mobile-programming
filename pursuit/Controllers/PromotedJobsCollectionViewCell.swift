52e680fa1c6c1702fa29b55c830e5470b7146a23
Mohammed-M8 <202200692@student.polytechnic.bh>
2024-12-08 08:38:41 +0300

    Added slight changes to job page along with fixes in navigation

---
+++ pursuit/Controllers/PromotedJobsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PromotedJobsCollectionViewCell.swift
+//  pursuit
+//
+//  Created by BP-36-201-17 on 08/12/2024.
+//
+
+import UIKit
+
+class PromotedJobsCollectionViewCell: UICollectionViewCell {
+    
+}
