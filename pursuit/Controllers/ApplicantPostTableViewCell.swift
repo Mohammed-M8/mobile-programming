60b4588a3e83229c13f4464552075c154b5910c4
Ali4lqassab <201900568@student.polytechnic.bh>
2024-12-17 15:52:37 +0300

    Completed Table view cell prototype for both the Iphone and Ipad layout (adaptive layout)

---
+++ pursuit/Controllers/ApplicantPostTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  ApplicantPostTableViewCell.swift
+//  pursuit
+//
+//  Created by Ali Alqassab on 16/12/2024.
+//
+
+import UIKit
+
+class ApplicantPostTableViewCell: UITableViewCell {
+
+    
+    
+    
+    @IBOutlet weak var applicantImg: UIImageView!
+    
+    @IBOutlet weak var applicantName: UILabel!
+    
+    @IBOutlet weak var applicantJobRole: UILabel!
+    
+    @IBOutlet weak var applicantLocation: UILabel!
+    
+    @IBOutlet weak var applicantHours: UILabel!
+    
+    @IBOutlet weak var applicantViewBtn: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        applicantImg.layer.cornerRadius = 35
+        applicantImg.layer.masksToBounds = true
+        applicantImg.clipsToBounds = true
+        
+        applicantViewBtn.layer.cornerRadius = 20
+        applicantViewBtn.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- pursuit/Controllers/ApplicationPostViewController.swift
+++ pursuit/Controllers/ApplicationPostViewController.swift
@@ -7,18 +7,22 @@
 
 import UIKit
 
-class ApplicationPostViewController: UIViewController {
-
-    
+class ApplicationPostViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var btnFilter: UIButton!
     @IBOutlet weak var btnSort: UIButton!
     
     @IBOutlet weak var imgProfile: UIImageView!
     
+    @IBOutlet weak var ApplicantPostTableView: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ApplicantPostTableView.dataSource = self
+        ApplicantPostTableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
@@ -27,6 +31,15 @@ class ApplicationPostViewController: UIViewController {
         btnFilter.layer.cornerRadius = 5
         btnSort.layer.cornerRadius = 5
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ApplicantPostTableView.dequeueReusableCell(withIdentifier: "applicantTableCell", for: indexPath) as! ApplicantPostTableViewCell
+        return cell
     }
 
     /*
