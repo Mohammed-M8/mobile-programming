360dbcb36e1ee61b80ddb6f4105f46a43dcee19d
Mohammed-M8 <202200692@student.polytechnic.bh>
2024-12-27 18:51:24 +0300

    Added dynamic data display to Article Paragraph controller

---
+++ pursuit/Controllers/JobSeekerViewControllers/Article Table Views/HeaderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderTableViewCell.swift
+//  pursuit
+//
+//  Created by BP-36-201-17 on 27/12/2024.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
